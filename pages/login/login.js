5345ed22241cca249136588b67249792452532f9
liqiang <mzgs@foxmail.com>
2018-05-10 22:44:47 +0800

    .

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,68 +5,73 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
 
-  toLogin:function(){
+  toLogin: function (res) {
+    if (!res.detail.userInfo) {
+      wx.showToast({ title: '登录失败，您已拒绝授权', icon: 'none' });
+      return;
+    }
+    console.log(res.detail.userInfo);
     wx.navigateTo({
-      url: '../home/home'
+      url: '../home/home',
     })
   }
 })
